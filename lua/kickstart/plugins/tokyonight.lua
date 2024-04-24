692feb972713cb823158d2f17614ec01fbdf672a
makurz <makurz@student.42heilbronn.de>
2024-04-24 23:27:57 +0200

    update tokyonight to proper config file

--- lua/kickstart/plugins/tokyonight.lua
+++ lua/kickstart/plugins/tokyonight.lua
@@ -5,11 +5,23 @@ return {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'folke/tokyonight.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
+      require('tokyonight').setup {
+        style = 'night',
+        transparent = true,
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+        },
+        on_colors = function(colors)
+          colors.hint = colors.orange
+          colors.error = "#ff0000"
+        end
+      }
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
