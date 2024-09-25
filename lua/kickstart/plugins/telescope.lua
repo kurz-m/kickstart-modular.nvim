3094d45ff16d8318ae2c286ce3da26a17e392308
kurmar <markus.kurz@sigmatek.at>
2024-09-25 13:55:06 +0200

    update telescope
    list all functions in current file for a better overview

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -87,6 +87,7 @@ return {
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+      vim.keymap.set('n', '<C-f>', builtin.lsp_document_symbols({ symbols='function' }), { desc = 'Find all symbols in current file' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
