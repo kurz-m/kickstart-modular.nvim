9605221174722838779e826276c7e57a72399efb
kurmar <m.kurz92@gmail.com>
2025-03-09 22:01:10 +0100

    remove pyright from lspconfig.lua
    Signed-off-by: kurmar <m.kurz92@gmail.com>

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -137,7 +137,6 @@ return {
         gopls = {},
         dockerls = {},
         bashls = {},
-        -- pyright = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
